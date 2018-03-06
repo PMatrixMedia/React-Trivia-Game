9f7ee8343dae8bfb3b916bddbc29d812dfe24a13
PMatrixMedia <33465098+PMatrixMedia@users.noreply.github.com>
2018-03-06 12:30:23 -0600

    Add files via upload
    navigate to the src folder
    run npm start
    
    Counter works, could not get timer to display
    Could get and track the answers to the questions however could not get the boolean function to work
    also tried to add an HTML front end with limited sucess.

---
+++ src/Question.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+function Question(props) {
+
+    return (
+        <h2 className="question">{props.content}</h2>
+    );
+}
+
+Question.propTypes = {
+    content: PropTypes.string.isRequired
+};
+
+export default Question;
