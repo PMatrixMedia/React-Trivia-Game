99d153867ccb74bd430a76f0ba2d13c72ddf26fd
Christopher Faison <pmatrixmedia@gmail.com>
2018-03-07 19:42:23 -0600

    Additional Source Files

---
+++ quizQuestions.js
@@ -0,0 +1,120 @@
+// JavaScript source code
+var AnswerResult = []
+
+
+
+var quizQuestions = [
+    {
+        //human
+        question: "What what is the name of Goku's son?",
+        answers: [
+            {
+                type: "Saiyan",
+                content: "Vegeta",
+                solution: "x",
+            },
+            {
+                type: "Namek",
+                content: "Dende",
+                solution: "x"
+            },
+            {
+                type: "Human",
+                content: "Gohan",
+                solution: "y",
+            }
+        ],
+    
+
+
+      //human
+      question: "Who is the primary enemy during the Cell Arc ?",
+        answers: [
+            {
+                type: "Saiyan",
+                content: "Brolly",
+                solution:"x",
+
+            },
+            {
+                type: "Namek",
+                content: "Piccolo",
+                solution:"x",
+            },
+            {
+                type: "Human",
+                content: "Androids",
+                solution:"y",
+            }
+        ],
+    
+
+          //saiyan
+          question: "Who is the primary Enemy in the beginning of Dragon Ball Z?",
+             answers: [
+            {
+                type: "Saiyan",
+                content: "Vegeta",
+                solution: "y",
+            },
+            {
+                type: "Namek",
+                content: "Planet Namek",
+                solution: "x",
+            },
+            {
+                type: "Human",
+                content: "Mr. Satan",
+                solution:"x",
+            }
+        ],
+    
+
+    //Namek
+    question: "Who is One of Goku's greatest enemies Turned Ally in Dragon Ball Super?",
+        answers: [
+            {
+                type: "Saiyan",
+                content: "Kefla",
+                solution: "x",
+            },
+            {
+                type: "Namek",
+                content: "Freeza",
+                solution: "y",
+            },
+            {
+                type: "Human",
+                content: "Krillan",
+                solution:"x",
+            }
+        ],
+    
+
+       //Namek
+       question: "What planet is the Freeza Arc Set in?",
+           answers: [
+            {
+                type: "Saiyan",
+                content: "Planet Vegeta",
+                solution: "x",
+            },
+            {
+                type: "Namek",
+                content: "Planet Namek",
+                solution:"y",
+            },
+            {
+                type: "Human",
+                content: "Earth Future",
+                solution:"x",
+            }
+        ],
+    },
+];
+
+//optionalArrayOf: PropTypes.arrayOf(quizQuestions.number){
+//   [x:]
+//}
+
+export default quizQuestions;
